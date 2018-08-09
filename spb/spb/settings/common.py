4bb87725458b63cf3c1dd5f3a8c37ae887d7afcd
HenryG <glueck.h@googlemail.com>
2018-08-09 23:10:47 +0200

    stted default value for secret key

--- spb/spb/settings/common.py
+++ spb/spb/settings/common.py
@@ -7,6 +7,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+SECRET_KEY = 'PLEASE_OVERWRITE_ME!'
+
 # Project apps
 PROJECT_APPS = [
     'api',
